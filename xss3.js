52183c810f5a170edb9d00fefff8bb7c07324f08
xssx <wymjid@163.com>
2012-12-12 16:49:48 +0800

    Update xss3.js

--- xss3.js
+++ xss3.js
@@ -1,1 +1,30 @@
-hello world
+//需要Base64见：http://www.webtoolkit.info/javascript-base64.html
+function make_base_auth(user, password) {
+  var tok = user + ':' + pass;
+  var hash = Base64.encode(tok);
+  return "Basic " + hash;
+} 
+
+var auth = make_basic_auth('QLeelulu','mypassword');
+var url = 'http://example.com'; 
+
+// 原始JavaScript
+xml = new XMLHttpRequest();
+xml.setRequestHeader('Authorization', auth);
+xml.open('GET',url) 
+
+// ExtJS
+Ext.Ajax.request({
+    url : url,
+    method : 'GET',
+    headers : { Authorization : auth }
+}); 
+
+// jQuery
+$.ajax({
+    url : url,
+    method : 'GET',
+    beforeSend : function(req) {
+        req.setRequestHeader('Authorization', auth);
+    }
+});
